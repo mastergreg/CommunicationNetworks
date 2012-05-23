41343a6da6df97e77f0fe62ad651a8e5c148431b
Gregory Liras <gregliras@gmail.com>
2012-05-23 08:30:55 +0300

    upadated awk.out

--- Ask5/master/ex5.tcl
+++ Ask5/master/ex5.tcl
@@ -18,7 +18,7 @@ set tcp0 [new Agent/TCP/Sack1]
 $tcp0 set window_ 4
 $tcp0 set windowInit_ 4
 $tcp0 set syn_ false
-$tcp0 set packetSize 1500
+$tcp0 set packetSize_ 1500
 $ns attach-agent $n(0) $tcp0
 
 set sink0 [new Agent/TCPSink]
