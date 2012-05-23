a36b1191300605bb75a0df2429cfe5776e78d5a0
Gregory Liras <gregliras@gmail.com>
2012-05-23 09:35:32 +0300

    added awk5

--- Ask5/master/ex5.tcl
+++ Ask5/master/ex5.tcl
@@ -12,11 +12,11 @@ set n(1) [$ns node]
 $ns at 0.0 "$n(0) label SRP_sender"
 $ns at 0.0 "$n(1) label SRP_reciever"
 
-$ns duplex-link $n(0) $n(1) 100Mb 50ms DropTail
+$ns duplex-link $n(0) $n(1) 10Mb 5ms DropTail
 
-set tcp0 [new Agent/TCP/Sack1]
-$tcp0 set window_ 82
-$tcp0 set windowInit_ 82
+set tcp0 [new Agent/TCP/Reno]
+$tcp0 set window_ 4
+$tcp0 set windowInit_ 4
 $tcp0 set syn_ false
 $tcp0 set packetSize_ 1500
 $ns attach-agent $n(0) $tcp0
